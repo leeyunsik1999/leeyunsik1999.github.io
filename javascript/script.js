dfc84f3429e0365d3576d18ac0c632a389012d14
Yunsik Edwin Lee <leeyunsik1999@gmail.com>
2022-01-21 11:34:05 -0500

    Buttons and placeholder logic works. TODO: Refactor utils.js, start working on actual wordle word logic

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,52 @@
 import * as utils from "./utils.js";
 
+// Initializing variables to keep track of which word row and which letter we are typing in.
 window.word_pointer = 1;
 window.letter_pointer = 1;
 
-
-window.addEventListener("load", () => {
+/**
+ * Function used for setting letterboxes to their default state. Also used to reset state.
+ */
+function loadLetterboxes() {
     let letterboxList = document.getElementsByClassName("letterbox");
-    for (const letterbox of letterboxList){
+
+    // Sets data and parameters for letterbox to original state. setLetterboxDefault called as this function is also used for resetting.
+    for (const letterbox of letterboxList) {
         letterbox.value = 0;
         letterbox.innerText = "";
+        utils.setLetterboxDefault(letterbox);
         letterbox.addEventListener("click", utils.letterboxClick);
     }
+}
+
+window.addEventListener("load", () => {
+    // Loads letterboxes to default state.
+    loadLetterboxes();
+    // Logic for typing in words.
     window.addEventListener("keydown", (event) => {
         // Filtering out non-letters
-        if (event.key.match(/[a-zA-Z]/)){
+        if (event.key.match(/[a-zA-Z]/)) {
             // Filtering out non-letters; shift/control/etc is not filtered by regex
-            if (event.key.length === 1){
+            if (event.key.length === 1) {
                 let letter = event.key.toUpperCase();
                 console.log(`Pressed ${letter}`);
-                if (letter_pointer < 6){
-                    let box = document.getElementById(`${word_pointer}-${letter_pointer}`);
-                    box.value = 1;
-                    box.style.backgroundColor = "#343434";
-                    box.style.border = "0px";
-                    box.innerText = letter;
+                if (letter_pointer < 6) {
+                    utils.setLetterboxEntered(document.getElementById(`${word_pointer}-${letter_pointer}`), letter);
                     letter_pointer++;
                 }
-            } else if (event.key === "Backspace" && letter_pointer > 1){
+            } else if (event.key === "Backspace" && letter_pointer > 1) {
                 letter_pointer--;
-                let box = document.getElementById(`${word_pointer}-${letter_pointer}`);
-                box.value = 0;
-                box.style.backgroundColor = "";
-                box.style.border = "2px solid darkgrey";
-                box.innerText = "";
+                utils.setLetterboxDefault(document.getElementById(`${word_pointer}-${letter_pointer}`));
             }
         }
-        utils.getWord();
+    });
+
+    document.getElementById("btn-submit").addEventListener("click", utils.submitWord);
+
+    // Reset button loads the letterboxes to default state again, and resets pointer values.
+    document.getElementById("btn-reset").addEventListener("click", () => {
+        loadLetterboxes();
+        window.word_pointer = 1;
+        window.letter_pointer = 1;
     });
 });
--- javascript/utils.js
+++ javascript/utils.js
@@ -1,12 +1,9 @@
-export function letterboxClick(){
-    console.log("dsadA");
-    if (this.value !== 0){
-        console.log("Here");
-        if (this.value === 1){
-            console.log("1");
+export function letterboxClick() {
+    if (this.value !== 0) {
+        if (this.value === 1) {
             this.value = 2;
             this.style.backgroundColor = "#b39c3c";
-        } else if (this.value === 2){
+        } else if (this.value === 2) {
             this.value = 3;
             this.style.backgroundColor = "#548b4c";
         } else {
@@ -16,10 +13,59 @@ export function letterboxClick(){
     }
 }
 
-export function getWord(){
+/**
+ * Sets the border, text content and color of letterbox to what it should be by default
+ * @param {DOM Element} box Should be letterbox
+ */
+export function setLetterboxDefault(box) {
+    box.value = 0;
+    box.style.backgroundColor = "";
+    box.style.border = "2px solid darkgrey";
+    box.innerText = "";
+}
+
+/**
+ * Sets the border, text content and color of the letterbox to what it should be when a letter is entered
+ * @param {DOM Element} box Should be letterbox
+ * @param {String} letter Letter for box's innertext to be set as
+ */
+export function setLetterboxEntered(box, letter) {
+    box.value = 1;
+    box.style.backgroundColor = "#343434";
+    box.style.border = "0px";
+    box.innerText = letter;
+}
+
+export function getWord() {
     let word = "";
-    for (let i = 1; i < 6; i++){
+    for (let i = 1; i < 6; i++) {
         word += document.getElementById(`${word_pointer}-${i}`).innerHTML;
     }
     console.log(word);
+}
+
+export function submitWord() {
+    if (letter_pointer !== 6){
+        alert("Please type in a full word!");
+    } else {
+        getWord();
+        for (let i = 1; i < 6; i++) {
+            let box = document.getElementById(`${word_pointer}-${i}`);
+
+            // Placeholder logic to interact with wordle AI that is to be implemented.
+            // Case of wrong letter
+            if (box.value === 1){
+                console.log(`${box.innerText} is not present in the answer.`);
+            // Case of correct letter in wrong position
+            } else if (box.value === 2){
+                console.log(`${box.innerText} is in the answer, but not in position ${i}.`);
+            // Case of correct letter in correct position
+            } else {
+                console.log(`${box.innerText} is the right letter in position ${i}.`);
+            }
+            box.removeEventListener("click", letterboxClick);
+        }
+        word_pointer++;
+        letter_pointer = 1;
+    }
 }
